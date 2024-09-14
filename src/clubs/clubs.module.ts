1f236d63cda9a6ac549cd98ae35966170b4fa24e
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2024-09-13 22:44:54 -0500

    feat: register typeorm entities

--- src/clubs/clubs.module.ts
+++ src/clubs/clubs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClubsService } from './clubs.service';
 import { ClubsController } from './clubs.controller';
+import { Club } from './entities/club.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Club])],
   providers: [ClubsService],
   controllers: [ClubsController],
 })
